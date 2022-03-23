1d19a1cc38bfb0dc4c246f4bf57b480812dd9cd8
LeonardoCGoulart <joedarkalone@hotmail.com>
2022-03-22 22:21:20 -0300

    adicionado funcionalidade POST

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// banco de dados em memória (poderia user DB)
+let books = []
+
+//criação do app
+const express = require('express')
+const app = express()
+app.use(express.json()) // passa a entender JSON
+
+//aplicação de middlewares
+app.post('/books', (req, res) => {
+    const {id, title, author, publishAt} = req.body
+    const book = { id, title, author, publishAt }
+    books.push(book)
+    return res.status(201).json(book);
+})
+
+//servidor rodando
+app.listen(3333, () => console.log("servidor esta rodando na porta 3333!"))
