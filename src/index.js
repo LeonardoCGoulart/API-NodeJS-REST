0a02eefd78ffc7bdd04024ad927d4c9c9544ef1f
LeonardoCGoulart <joedarkalone@hotmail.com>
2022-03-22 22:37:18 -0300

    adicionado funcionalidaded GET especifico

--- src/index.js
+++ src/index.js
@@ -18,5 +18,11 @@ app.get('/books', (req, res) => {
     return res.status(200).json(books)
 })
 
+app.get('/books/:book_id', (req,res) => {
+    const {book_id} = req.params
+    const book = books.find((book) => book.id === book_id)
+    return res.status(200).json(book);
+})
+
 //servidor rodando
 app.listen(3333, () => console.log("servidor esta rodando na porta 3333!"))
